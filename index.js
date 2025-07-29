f33a76278c39a78f31cdd2eb9d011ee3738b61ec
joymahata22 <113142254+joymahata22@users.noreply.github.com>
2025-07-30 01:05:29 +0530

    second commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://react-assignment-frontend-pi.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
